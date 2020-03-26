e6e62116e64c88187d47c63a41166f3e93b8bf1a
ken-25 <dknndn8@gmail.com>
2020-03-26 22:02:56 +0900

    destroy,update

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -18,6 +18,20 @@ class PostController < ApplicationController
   end
 
   def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post "posts/create" => "post#create"
   get "posts/:id" => "post#show"
   get "posts/:id/edit" => "post#edit"
+  post "posts/:id/update" => "post#update"
+  post "posts/:id/destroy" => "post#destroy"
   get "/" => "home#top"
   get "about" => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
